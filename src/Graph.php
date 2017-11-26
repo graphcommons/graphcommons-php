2118acf43d24710807ada0421a737b4d4472c785
Kerem Güneş <k-gun@mail.com>
2017-11-26 05:26:30 +0300

    Add graph statuses to Graph object, Move signals to Graph object.

--- src/Graph.php
+++ src/Graph.php
@@ -6,6 +6,21 @@ use \stdClass as object; // @note This will be forbidden with PHP/7.2.
 
 final class Graph
 {
+    public const STATUS_DRAFT     = 0,
+                 STATUS_PUBLISHED = 1,
+                 STATUS_PRIVATE   = 2;
+
+    public const SIGNAL_CREATE_NODE      = 'node_create',
+                 SIGNAL_CREATE_EDGE      = 'edge_create',
+                 SIGNAL_DELETE_NODE      = 'node_delete',
+                 SIGNAL_DELETE_EDGE      = 'edge_delete',
+                 SIGNAL_UPDATE_NODE      = 'node_update',
+                 SIGNAL_UPDATE_EDGE      = 'edge_update',
+                 SIGNAL_UPDATE_NODE_TYPE = 'nodetype_update',
+                 SIGNAL_UPDATE_EDGE_TYPE = 'edgetype_update',
+                 SIGNAL_DELETE_NODE_TYPE = 'nodetype_delete',
+                 SIGNAL_DELETE_EDGE_TYPE = 'edgetype_delete';
+
     private $api;
     private $data = [];
 
