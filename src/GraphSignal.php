168e095b7d804766b19fdf8bfe2e25a2445a114f
Kerem Güneş <k-gun@mail.com>
2017-11-26 05:15:19 +0300

    Fix signals.

--- src/GraphSignal.php
+++ src/GraphSignal.php
@@ -4,14 +4,14 @@ namespace GraphCommons;
 
 final class GraphSignal
 {
-    public const CREATE_NODE      = 'create_node',
-                 CREATE_EDGE      = 'create_edge',
-                 DELETE_NODE      = 'delete_node',
-                 DELETE_EDGE      = 'delete_edge',
-                 UPDATE_NODE      = 'update_node',
-                 UPDATE_EDGE      = 'update_edge',
-                 UPDATE_NODE_TYPE = 'update_node_type',
-                 UPDATE_EDGE_TYPE = 'update_edge_type',
-                 DELETE_NODE_TYPE = 'delete_node_type',
-                 DELETE_EDGE_TYPE = 'delete_edge_type';
+    public const CREATE_NODE      = 'node_create',
+                 CREATE_EDGE      = 'edge_create',
+                 DELETE_NODE      = 'node_delete',
+                 DELETE_EDGE      = 'edge_delete',
+                 UPDATE_NODE      = 'node_update',
+                 UPDATE_EDGE      = 'edge_update',
+                 UPDATE_NODE_TYPE = 'nodetype_update',
+                 UPDATE_EDGE_TYPE = 'edgetype_update',
+                 DELETE_NODE_TYPE = 'nodetype_delete',
+                 DELETE_EDGE_TYPE = 'edgetype_delete';
 }
