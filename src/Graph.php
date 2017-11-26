e7c4e996488807705c30e3b03242534531a0a424
Kerem Güneş <k-gun@mail.com>
2017-11-26 06:53:05 +0300

    Complete Graph methods [/graphs/*].

--- src/Graph.php
+++ src/Graph.php
@@ -27,21 +27,78 @@ final class Graph
     public function __construct(Api $api, array $data = [])
     {
         $this->api = $api;
-        $this->data = $data;
     }
 
     public function check(string $id): bool
     {
-        return $this->api->getClient()->head('/graphs/' . $id)->getResponse()->code == 200;
+        return $this->api->getClient()->head('/graphs/'. $id)
+            ->getResponse()->code == 200;
     }
 
     public function get(string $id): ?object
     {
-        return $this->api->getClient()->get('/graphs/' . $id)->getResponse()->body;
+        return $this->api->getClient()->get('/graphs/'. $id)
+            ->getResponse()->body;
     }
 
-    public function create(array $data): ?object
+    public function create(array $body): ?object
     {
-        return $this->api->getClient()->post('/graphs', ['body' => $data])->getResponse()->body;
+        return $this->api->getClient()->post('/graphs', ['body' => $body])
+            ->getResponse()->body;
+    }
+
+    public function update(string $id, array $graph): ?object
+    {
+        return $this->api->getClient()->put('/graphs/'. $id, ['body' => ['graph' => $graph]])
+            ->getResponse()->body;
+    }
+
+    public function clear(string $id): ?object
+    {
+        return $this->api->getClient()->put('/graphs/'. $id .'/clear')
+            ->getResponse()->body;
+    }
+
+    public function createSignal(string $id, array $signals): ?object
+    {
+        return $this->api->getClient()->put('/graphs/'. $id .'/add', ['body' => ['signals' => $signals]])
+            ->getResponse()->body;
+    }
+
+    public function getTypes(string $id): ?object
+    {
+        return $this->api->getClient()->get('/graphs/'. $id .'/types')
+            ->getResponse()->body;
+    }
+
+    public function getEdges(string $id, array $uriParams): ?object
+    {
+        return $this->api->getClient()->get('/graphs/'. $id .'/edges', ['uriParams' => $uriParams])
+            ->getResponse()->body;
+    }
+
+    public function getPaths(string $id, array $uriParams): ?object
+    {
+        return $this->api->getClient()->get('/graphs/'. $id .'/paths', ['uriParams' => $uriParams])
+            ->getResponse()->body;
+    }
+
+    public function getCollabFilter(string $id, array $uriParams): ?object
+    {
+        return $this->api->getClient()->get('/graphs/'. $id .'/collab_filter', ['uriParams' => $uriParams])
+            ->getResponse()->body;
+    }
+
+    public function search(string $query, array $uriParams): ?array
+    {
+        return $this->api->getClient()->get('/graphs/search', ['uriParams' => ['query' => $query] + $uriParams])
+            ->getResponse()->body;
+    }
+
+    // @note API side does not return body yet (reported).
+    public function delete(string $id): ?object
+    {
+        return $this->api->getClient()->delete('/graphs/'. $id)
+            ->getResponse()->body;
     }
 }
